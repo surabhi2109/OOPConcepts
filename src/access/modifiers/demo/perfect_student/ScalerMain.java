23fa4d9a5390bc318698d81d44d918bff803b3c0
Surabhi Korgaonkar <surabhi.korgaonkar@gmail.com>
2023-08-01 13:35:10 +0530

    default and public class - 01/08/2023

---
+++ src/access/modifiers/demo/perfect_student/ScalerMain.java
@@ -0,0 +1,7 @@
+package access.modifiers.demo.perfect_student;
+
+import access.modifiers.demo.extra_ordinary_student.Scaler;
+
+public class ScalerMain {
+    //Scaler obj = new Scaler();    -- default class cannot be accessed in different package
+}
